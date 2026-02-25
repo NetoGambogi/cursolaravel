d51084afb5ef76b5f673f316bd1032c850566e16
NetoGambogi <netogambogi@hotmail.com>
2026-02-25 11:14:44 -0300

    inicio livewire

---
+++ laravel_studies_13_livewire/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.main-layout>
+    
+    <div class="display-6 text-center">Livewire</div>
+    <hr>
+
+    @livewire('counter')
+
+</x-layouts.main-layout>
---
+++ laravel_studies_13_livewire/resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div class="d-flex justify-content-around gap-5 mb-3">
+    
+    <button wire:click="decrement" class="btn btn-primary px-5">Decrement</button>
+
+    <h1>Count: {{$count}}</h1>
+
+    <button wire:click="increment" class="btn btn-primary px-5">Increment</button>
+</div>
