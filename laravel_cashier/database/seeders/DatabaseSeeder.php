cd9b5478a3907f1a4667e268690b4fb1771a7cf2
NetoGambogi <netogambogi@hotmail.com>
2026-03-15 12:54:46 -0300

    Início laravel cashier

---
+++ laravel_cashier/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        for($id = 1; $id <=3; $id++){
+            DB::table('users')->insert([
+                'name' => "User $id",
+                'email' => "user_$id@gmail.com",
+                'password' => bcrypt('masterkey'),
+                'email_verified_at' => now(),
+                'created_at' => now()
+            ]);
+        }
+    }
+}
