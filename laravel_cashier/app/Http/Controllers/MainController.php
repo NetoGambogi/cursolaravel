2b2409079af28633048fa2fbe495800f201d16b3
NetoGambogi <netogambogi@hotmail.com>
2026-03-16 11:13:48 -0300

    Controllers, controle de acesso, conta no stripe

---
+++ laravel_cashier/app/Http/Controllers/MainController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function loginPage()
+    {
+        return view('login');
+    }
+
+    public function loginSubmit($id)
+    {
+        //login direto
+        $user = User::findOrFail($id);
+
+        if($user){
+            auth()->login($user);
+            return redirect()->route('plans');
+        }
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return redirect()->route('login');
+    }
+
+    public function plans()
+    {
+        return view('plans');
+    }
+}
